15707442b8aedc89489c04e673acda0f9684853b
achref ouerfelli <achref.ouerfelli@with.fr>
2024-05-16 17:02:51 +0100

    deploy

---
+++ src/JS/Actions/FilmActions.js
@@ -0,0 +1,37 @@
+import axios from "axios"
+import { ADD_FILM, DELETE_FILM, FAIL_FILM, GET_FILMS, LOAD_FILM } from "../ActionTypes/ActionsTypes"
+
+
+
+export const addFilm =(newFilm)=>async(dispatch) =>{
+    dispatch({type:LOAD_FILM})
+    try {
+        let result = await axios.post('https://mernproject-sj58.onrender.com/api/film/addFilm', newFilm)
+        dispatch({type:ADD_FILM, payload : result.data})
+    } catch (error) {
+        dispatch({type: FAIL_FILM, payload: error.response})
+    }
+    }
+
+    //find film
+export const getFilms =()=>async (dispatch) =>{
+    dispatch({type:LOAD_FILM})
+    try {
+        let result =await axios.get('https://mernproject-sj58.onrender.com/api/film/films')
+        dispatch({type:GET_FILMS, payload: result.data})
+    } catch (error) {
+        dispatch({type:FAIL_FILM,payload:error.response})
+    }
+}
+
+//delete announce
+export const deleteFilm =(id) =>async (dispatch)=>{
+    dispatch({type:LOAD_FILM})
+    try {
+       let result=await axios.delete(`https://mernproject-sj58.onrender.com/api/film/deleteFilm/${id}`)
+        dispatch({type:DELETE_FILM,payload:result.data})
+        
+    } catch (error) {
+        dispatch({type:FAIL_FILM,payload:error.response})
+    }
+}
